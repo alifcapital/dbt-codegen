0faa613e57f745c232b3c9d69bb7557a5b00e3bd
amirjonr <amirshorakhimov0017gmail.com>
2025-05-21 17:17:16 +0500

    added starrocks adapter macroses

--- macros/generate_source.sql
+++ macros/generate_source.sql
@@ -85,3 +85,72 @@
 {% endif %}
 
 {% endmacro %}
+
+{% macro starrocks__generate_source(schema_name, database_name, generate_columns, include_descriptions, include_data_types, table_pattern, exclude, name, table_names, include_database, include_schema, case_sensitive_databases, case_sensitive_schemas, case_sensitive_tables, case_sensitive_cols) %}
+
+{% set sources_yaml=[] %}
+{% do sources_yaml.append('version: 2') %}
+{% do sources_yaml.append('') %}
+{% do sources_yaml.append('sources:') %}
+{% do sources_yaml.append('  - name: ' ~ database_name | lower) %}
+
+{% if include_descriptions %}
+    {% do sources_yaml.append('    description: ""' ) %}
+{% endif %}
+
+{% if include_database %}
+{% do sources_yaml.append('    database: ' ~ (database_name if case_sensitive_databases else database_name | lower)) %}
+{% endif %}
+
+{% if schema_name != name or include_schema %}
+{% set full_schema = schema_name ~ '.' ~ database_name %}
+{% do sources_yaml.append('    schema: ' ~ (full_schema if case_sensitive_schemas else full_schema | lower)) %}
+{% endif %}
+
+{% do sources_yaml.append('    tables:') %}
+
+{% if table_names is none %}
+{% set tables=codegen.get_tables_in_schema(schema_name, database_name, table_pattern, exclude) %}
+{% else %}
+{% set tables = table_names %}
+{% endif %}
+
+{% for table in tables %}
+    {% do sources_yaml.append('      - name: ' ~ (table if case_sensitive_tables else table | lower) ) %}
+    {% if include_descriptions %}
+        {% do sources_yaml.append('        description: ""' ) %}
+    {% endif %}
+    {% if generate_columns %}
+    {% do sources_yaml.append('        columns:') %}
+
+        {% set table_relation=api.Relation.create(
+            schema=schema_name,
+            identifier=table
+        ) %}
+
+        {% set columns=adapter.get_columns_in_relation(table_relation) %}
+
+        {% for column in columns %}
+            {% do sources_yaml.append('          - name: ' ~ (column.name if case_sensitive_cols else column.name | lower)) %}
+            {% if include_data_types %}
+                {% do sources_yaml.append('            data_type: ' ~ codegen.data_type_format_source(column)) %}
+            {% endif %}
+            {% if include_descriptions %}
+                {% do sources_yaml.append('            description: ""' ) %}
+            {% endif %}
+        {% endfor %}
+            {% do sources_yaml.append('') %}
+
+    {% endif %}
+
+{% endfor %}
+
+{% if execute %}
+
+    {% set joined = sources_yaml | join ('\n') %}
+    {{ print(joined) }}
+    {% do return(joined) %}
+
+{% endif %}
+
+{% endmacro %}
