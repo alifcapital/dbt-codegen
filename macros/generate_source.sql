a93ad2de8fa7517e76973e17806770e73cd2d0ed
amirjonr <amirshorakhimov0017gmail.com>
2025-05-21 13:43:31 +0500

    Revert "configured for starrocks"
    This reverts commit f371cbeed751c0484870943d808df359bedcd568.

--- macros/generate_source.sql
+++ macros/generate_source.sql
@@ -1,25 +1,23 @@
-{% macro get_tables_in_schema(schema_name, database_name=target.database) %}
-    {% set sql %}
-        SELECT table_name
-        FROM iceberg.information_schema.tables
-        WHERE lower(table_catalog) = lower('{{ database_name }}')
-          AND lower(table_schema) = lower('{{ schema_name }}')
-        ORDER BY table_name
-    {% endset %}
+{% macro get_tables_in_schema(schema_name, database_name=target.database, table_pattern='%', exclude='') %}
 
-    {% set results = run_query(sql) %}
-    {% if execute %}
-        {% set table_list = results.columns[0].values() %}
-        {{ return(table_list | sort) }}
-    {% endif %}
+    {% set tables=dbt_utils.get_relations_by_pattern(
+        schema_pattern=schema_name,
+        database=database_name,
+        table_pattern=table_pattern,
+        exclude=exclude
+    ) %}
+
+    {% set table_list= tables | map(attribute='identifier') %}
+
+    {{ return(table_list | sort) }}
+
 {% endmacro %}
 
-{% macro generate_source(schema_name, catalog_name, database_name=target.database, generate_columns=False, include_descriptions=False, include_data_types=True, table_pattern='%', exclude='', name=schema_name, table_names=None, include_database=False, include_schema=False, case_sensitive_databases=False, case_sensitive_schemas=False, case_sensitive_tables=False, case_sensitive_cols=False) %}
-    {{ return(codegen.generate_source_starrocks(schema_name, database_name, catalog_name, generate_columns, include_descriptions, include_data_types, table_pattern, exclude, name, table_names, include_database, include_schema, case_sensitive_databases, case_sensitive_schemas, case_sensitive_tables, case_sensitive_cols)) }}
+{% macro generate_source(schema_name, database_name=target.database, generate_columns=False, include_descriptions=False, include_data_types=True, table_pattern='%', exclude='', name=schema_name, table_names=None, include_database=False, include_schema=False, case_sensitive_databases=False, case_sensitive_schemas=False, case_sensitive_tables=False, case_sensitive_cols=False) %}
+    {{ return(adapter.dispatch('generate_source', 'codegen')(schema_name, database_name, generate_columns, include_descriptions, include_data_types, table_pattern, exclude, name, table_names, include_database, include_schema, case_sensitive_databases, case_sensitive_schemas, case_sensitive_tables, case_sensitive_cols)) }}
 {% endmacro %}
 
-
-{% macro generate_source_starrocks(schema_name, database_name, catalog_name, generate_columns, include_descriptions, include_data_types, table_pattern, exclude, name, table_names, include_database, include_schema, case_sensitive_databases, case_sensitive_schemas, case_sensitive_tables, case_sensitive_cols) %}
+{% macro default__generate_source(schema_name, database_name, generate_columns, include_descriptions, include_data_types, table_pattern, exclude, name, table_names, include_database, include_schema, case_sensitive_databases, case_sensitive_schemas, case_sensitive_tables, case_sensitive_cols) %}
 
 {% set sources_yaml=[] %}
 {% do sources_yaml.append('version: 2') %}
@@ -31,16 +29,20 @@
     {% do sources_yaml.append('    description: ""' ) %}
 {% endif %}
 
-{# В StarRocks schema = catalog.database #}
-{% set full_schema = catalog_name ~ '.' ~ schema_name %}
-{% do sources_yaml.append('    schema: ' ~ full_schema) %}
+{% if database_name != target.database or include_database %}
+{% do sources_yaml.append('    database: ' ~ (database_name if case_sensitive_databases else database_name | lower)) %}
+{% endif %}
+
+{% if schema_name != name or include_schema %}
+{% do sources_yaml.append('    schema: ' ~ (schema_name if case_sensitive_schemas else schema_name | lower)) %}
+{% endif %}
 
 {% do sources_yaml.append('    tables:') %}
 
 {% if table_names is none %}
-    {% set tables=codegen.get_tables_in_schema(schema_name, database_name) %}
+{% set tables=codegen.get_tables_in_schema(schema_name, database_name, table_pattern, exclude) %}
 {% else %}
-    {% set tables = table_names %}
+{% set tables = table_names %}
 {% endif %}
 
 {% for table in tables %}
@@ -75,7 +77,8 @@
 {% endfor %}
 
 {% if execute %}
-    {% set joined = sources_yaml | join('\n') %}
+
+    {% set joined = sources_yaml | join ('\n') %}
     {{ print(joined) }}
     {% do return(joined) %}
 
